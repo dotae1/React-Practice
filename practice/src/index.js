bf6cd456da4b17c972007527a86c127d3e036138
최다빈 <9636515@gmail.com>
2026-03-16 22:20:42 +0900

    React Component실습코드

--- practice/src/index.js
+++ practice/src/index.js
@@ -1,39 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'; 
-//import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-const num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-const element = (
-<div style={{ display: "flex" }}>
-  {num.map(
-    (n) =>
-      n>=2 &&
-      n!==5 && (
-      <div
-        style={{
-          padding:10,
-          color:n % 2 ? 'blue' : 'black'
-        }}
-      >
-        {num.map((m) => (
-          <div>
-            {n} x {m} = {n * m}
-          </div>
-        ))} 
-      </div>
-    )
-  )}
-</div>
-);
-
 
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
 
-root.render(element);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
