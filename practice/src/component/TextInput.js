76da46851630aaaeb9ccc4fd4e68469d4e60e03c
최다빈 <9636515@gmail.com>
2026-03-21 23:20:38 +0900

    이벤트 관련 실습코드 업로드

---
+++ practice/src/component/TextInput.js
@@ -0,0 +1,12 @@
+function TextInput({value, setValue}) {
+    return (
+    <input type="text" 
+    value={value} 
+    onChange={(e) => {
+        setValue(e.target.value);
+    }} 
+    />
+    );
+}
+
+export default TextInput;
